f0098c55ed86d3bf9d3f9fd26c298288f752d2a8
Charles Gagnon <80009093+charlesgagnonChamplain@users.noreply.github.com>
2023-04-05 14:47:39 -0400

    Add files via upload
    initial commit, some work on shell script

---
+++ genmap.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# set the default map dimensions
+WIDTH=50
+HEIGHT=50
+
+# check if command line arguments were provided
+if [[ $# -eq 1 ]]; then
+    WIDTH=$1
+elif [[ $# -eq 2 ]]; then
+    WIDTH=$1
+    HEIGHT=$2
+fi
+
+# define function to generate a random ASCII character
+function genchar() {
+    printf \\$(printf '%03o' $((RANDOM % 93 + 33)))
+}
+
+# set the maximum number of map generation attempts
+MAX_ATTEMPTS=10
+attempts=0
+
+# loop until a valid map is generated or the maximum attempts are reached
+while [[ $attempts -lt $MAX_ATTEMPTS ]]; do
+    # create an empty map
+    map=''
+
+    # fill the map with random ASCII characters
+    for (( y=1; y<=$HEIGHT; y++ )); do
+        # create a temporary line to count blank spaces
+        line=''
+        for (( x=1; x<=$WIDTH; x++ )); do
+            line+="$(genchar)"
+        done
+        # count the number of blank spaces in the line
+        num_blanks=$(echo "$line" | tr -cd '[:blank:]' | wc -c)
+        # check if the line has enough non-blank characters
+        if [[ $num_blanks -ge $(($WIDTH/2)) ]]; then
+            map+="$line\n"
+        else
+            # force regeneration of the entire map
+            break
+        fi
+    done
+
+    # check if the map has enough non-blank characters
+    num_blanks=$(echo "$map" | tr -cd '[:blank:]' | wc -c)
+    if [[ $num_blanks -lt $(($WIDTH * $HEIGHT / 2)) ]]; then
+        # print the map to STDOUT and exit
+        echo -e "$map"
+        exit 0
+    else
+        # increment the number of attempts
+        attempts=$((attempts+1))
+    fi
+done
+
+# if the maximum number of attempts is reached, print an error message to STDERR and exit with error code
+echo "Error: could not generate a map with at least 50% non-blank characters after $MAX_ATTEMPTS attempts" >&2
+exit 1
+
