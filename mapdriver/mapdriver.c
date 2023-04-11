68eb7a19bd6e78e0bf9647e8ebcb42a50532320f
Charles Gagnon <80009093+charlesgagnonChamplain@users.noreply.github.com>
2023-04-11 18:49:11 -0400

    steps 1 and 2, possibly more

---
+++ mapdriver/mapdriver.c
@@ -0,0 +1,69 @@
+/****
+ * main.c
+ *
+ * The ASCII character device driver mini test
+ *
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include "common.h"
+
+int main(argc, argv)
+	int   argc;
+	char* argv[];
+{
+    char buf[BSIZE];
+	char *bufPtr = buf;
+    int fd, i, j, n;
+
+	if((fd = open("/dev/asciimap", O_RDWR)) >= 0)
+	{
+		for(i = 10; i <= 60; i += 5)
+		{
+			n = read(fd, buf, i);
+
+			if(i != n)
+			{
+				(void) fprintf(stderr, "*** wanted %d bytes, got %d\n", i, n);
+
+				if(n < 0)
+					perror("read(asciimap) failed");
+			}
+			else
+			{
+				(void) printf("received:  ");
+				for (j = 0; j < n; j++) (void) printf("%c ", buf[j]);
+				(void) printf("\n");
+			}
+
+			if(i == 30)
+			{
+				n = write(fd, ptr, BSIZE);
+                bytes_written += n;
+
+				char *newline = strchr(ptr, '\n');
+                width = newline - ptr + 1;
+                height = bytes_written / width;
+
+				if(n < 0)
+					perror("write(/dev/ASCII) failed");
+			}
+		}
+
+		close(fd);
+	}
+	else
+	{
+		perror("open(/dev/asciimap) failed");
+		exit(1);
+	}
+
+    exit(0);
+}
+
+/* EOF */
