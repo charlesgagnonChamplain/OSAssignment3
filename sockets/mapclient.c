a8ebd3e66e1a0262bea56661a6522b7aee8f2414
Ananda <ananda.shumockbailey@mymail.champlain.edu>
2023-04-30 16:52:03 -0400

    updated

--- sockets/mapclient.c
+++ sockets/mapclient.c
@@ -23,14 +23,23 @@ void print_map(char *map, int width, int height)
 	}
 }
 
+void cli_err(const char *err_msg)
+{
+    perror(err_msg);
+    exit(0);
+}
+
 int main (int argc, char *argv[])
 {
 	int sockfd = 0, n = 0;
 	struct sockaddr_in serv_addr;
+    client_map_req request;
+    int usr_height, usr_width;
 
 	int port = PORT;
 	char* ip = IP;
-	int width, height;
+	request.height = 0;
+    request.width = 0;
 
 	if(argc == 2)
 	{
@@ -38,62 +47,47 @@ int main (int argc, char *argv[])
 	}
 
 	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("\n Error : Could not create socket \n");
-        return 1;
-    } 
+        cli_err("\n Error : Could not create socket \n");
 
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(port);
 
     if (inet_pton(AF_INET, ip, &serv_addr.sin_addr) <= 0)
-    {
-        printf("\nError : Could not add IP");
-        return 1;
-    }
+        cli_err("\nError : Could not add IP");
 
     if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-       printf("\n Error : Connect Failed \n");
-       return 1;
-    } 
+        cli_err("\n Error : Connect Failed \n");
 
-	printf("Enter width and height (separated by a space)");
+	printf("Enter   width and height (separated by a space)");
     printf("If no customization of size is desired, enter 0 for both: ");
-    scanf("%d %d", &width, &height);
+    scanf("%d %d", &usr_width, &usr_height);
 
-    char reqBuff[3];
-    reqBuff[0] = 'M';
+    char message[2];
+    snprintf(message, 2, "%c", 'M');
+    write(sockfd, message, 1);
 
-    if(width == 0 && height == 0)
+    if(usr_width != 0 && usr_height != 0)
     {
-        reqBuff[1] = 0;
-        write(sockfd, reqBuff, 2);
+        request.width = usr_width;
+        request.height = usr_height;
     }
-    else
-    {
-        reqBuff[1] = width;
-        reqBuff[2] = height;
-        write(sockfd, reqBuff, 3);
-    }
+
+    n = write(sockfd, request, sizeof(request));
+    if (n < 0)
+        cli_err("Error : Could not write to socket");
 
 	char response;
-	if (read(sockfd, &response, 1) != 1)
-	{
-		fprintf(stderr, "\nError : Failed to read response type\n");
-		close(sockfd);
-		return 1;
-	}
+    (read(sockfd, &response, 1);
 
 	if (response == 'M')
 	{
 		server_map_resp serverMap;
 		read(sockfd, &serverMap, sizeof(serverMap));
 		int buffSize = serverMap.width * serverMap.height;
-		char *mapBuff = malloc(buffSize + 1);
+		char *mapBuff = malloc(buffSize);
 		
-		while((n = read(sockfd, mapBuff, sizeof(mapBuff))) > 0)
+		while((n = read(sockfd, mapBuff, buffSize - 1)) > 0)
 		{
 			mapBuff[n] = 0;
 			printf("%s", mapBuff);
@@ -111,9 +105,9 @@ int main (int argc, char *argv[])
 		server_err_resp serverErr;
 		read(sockfd, &serverErr, sizeof(serverErr));
 		int buffSize = serverErr.err_len;
-		char *errBuff = malloc(buffSize + 1);
+		char *errBuff = malloc(buffSize);
 		
-		while((n = read(sockfd, errBuff, sizeof(errBuff))) > 0)
+		while((n = read(sockfd, errBuff, buffSize - 1)) > 0)
 		{
 			errBuff[n] = 0;
 			fprintf(stderr, "%s\n", errBuff);
@@ -127,5 +121,4 @@ int main (int argc, char *argv[])
 
 	close(sockfd);
 	return 0;
-	
 }
